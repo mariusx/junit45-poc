069805b968d7597c0c70221d2e7b0c84aa42076f
Marius Jakobsen <marius@bokbasen.no>
2021-09-17 19:36:39 +0200

    POC junit4+5 based on https://github.com/apache/maven-surefire/tree/master/surefire-its/src/test/resources/junit-4-5

---
+++ src/test/java/biz/marius/junit/JUnit4Test.java
@@ -0,0 +1,10 @@
+package biz.marius.junit;
+
+import org.junit.Test;
+
+public class JUnit4Test {
+    @Test
+    public void test() {
+        System.out.println("Junit4 = " + true);
+    }
+}
---
+++ src/test/java/biz/marius/junit/JUnit5Test.java
@@ -0,0 +1,10 @@
+package biz.marius.junit;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5Test {
+    @Test
+    void test() {
+        System.out.println("Junit5 = " + true);
+    }
+}
